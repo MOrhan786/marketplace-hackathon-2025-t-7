10601ce448e86127dde888277acf9f57847b6c40
Fatima <fatima787asif@gmail.com>
2025-02-12 19:59:43 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Herosection from './component/Herosection';
+import Section1 from './component/Section1';
+import Section2 from './component/Section2';
+
+const page = () => {
+  return (
+    <>
+    <Herosection />
+    <Section1 />
+    <Section2 />
+    </>
+  )
+}
+
+export default page
---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity';
+import recommendedcar from './recommendedcar';
+import popularcar from './popularcar';
+
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [popularcar , recommendedcar]
+}
